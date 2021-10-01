df232b0933e99d48ffadd4513a19fb94223a2542
UmakanthSahu <69105946+UmakanthSahu@users.noreply.github.com>
2021-10-01 22:29:59 +0530

    Update Factorial.java
    added Recursive and Iterative functions

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
 class Factorial {
-    int fact(int n, int result){
-        if(n==1){
+    int factRec(int n, int result){
+        if(n==1)
             return result;
-        }
-        else{
+        
         return(fact(n-1,n*result));
     }
-}
+    
+    int factRec(int n){
+        if(n == 1 || n == 0)
+            return 1;
+        return n * fact(n-1);
+    }
+    
+    int fact(int n){
+        int ans = 1;
+        
+        for(int i=2;i<n;i++)
+            ans *= i;
+        
+        return ans;
+    }
 }
 class FactorialDemo{
     public static void main(String args[]){
@@ -15,6 +28,8 @@ class FactorialDemo{
         Factorial f=new Factorial();
         System.out.print("Enter a number ");
         int n=sc.nextInt();
-        System.out.println("Factorial of "+n+" is "+f.fact(n,1));
+        System.out.println("Factorial of "+n+" is "+f.fact(n));
+        System.out.println("Factorial of "+n+" using Recusion is "+f.factRec(n,1));
+        
     }
 }
